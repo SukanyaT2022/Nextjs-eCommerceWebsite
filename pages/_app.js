eed1ad7ab72fd6632422cc8134d3fd90a8cc5d2c
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-11-21 12:55:41 -0500

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from '../components/Navbar'
+import Navbar2 from "@/components/Navbar2";
 
 export default function App({ Component, pageProps }) {
   return(
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Navbar2/>
       <Component {...pageProps} />
     </div>
 )}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../components/Navbar";
+import { Alert } from "flowbite-react";
+import { FaBeer } from 'react-icons/fa';
 
 export default function Home() {
   return (
@@ -8,7 +10,7 @@ export default function Home() {
       <Head>
           <title>Home</title>
       </Head>
-      <h1 className="text-4xl text-center mt-12">Welcome to Home Page</h1>
+      <h1 className="text-4xl text-center mt-12">Welcome to Home Page <FaBeer/></h1>
     </div>
   );
 }
